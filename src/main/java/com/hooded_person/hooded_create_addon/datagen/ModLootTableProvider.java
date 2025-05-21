ea3b6fdf0139fc49f85c0452eb3fd374019ce743
Niels Groen <hooded.main@gmail.com>
2025-05-21 13:25:52 +0200

    Added java classes for data generation

---
+++ src/main/java/com/hooded_person/hooded_create_addon/datagen/ModLootTableProvider.java
@@ -0,0 +1,15 @@
+package com.hooded_person.hooded_create_addon.datagen;
+
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricBlockLootTableProvider;
+
+public class ModLootTableProvider extends FabricBlockLootTableProvider {
+	public ModLootTableProvider(FabricDataOutput dataOutput) {
+		super(dataOutput);
+	}
+
+	@Override
+	public void generate() {
+
+	}
+}
---
+++ src/main/java/com/hooded_person/hooded_create_addon/datagen/ModModelProvider.java
@@ -0,0 +1,22 @@
+package com.hooded_person.hooded_create_addon.datagen;
+
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricModelProvider;
+import net.minecraft.data.models.BlockModelGenerators;
+import net.minecraft.data.models.ItemModelGenerators;
+
+public class ModModelProvider extends FabricModelProvider {
+	public ModModelProvider(FabricDataOutput output) {
+		super(output);
+	}
+
+	@Override
+	public void generateBlockStateModels(BlockModelGenerators blockModelGenerators) {
+
+	}
+
+	@Override
+	public void generateItemModels(ItemModelGenerators itemModelGenerators) {
+
+	}
+}
